999ce21ba326633588a6fbf24da00c382a5d6801
Jamie Wong <jlfwong@figma.com>
2017-11-12 10:30:53 -0800

    Start of mini-profiler UI revamp

--- lib/html/mini-profiler.tsx
+++ lib/html/mini-profiler.tsx
@@ -9,6 +9,7 @@ interface Result {
 
 interface Options {
   path: string
+  currentId: string
 }
 
 type XhrOpenListener = (xhr: XMLHttpRequest) => void
@@ -86,42 +87,160 @@ function getOptions(script: HTMLScriptElement) {
   };
 }
 
-class MiniProfiler extends Component<void, void> {
-  componentDidMount() {
-    const script = document.getElementById('mini-profiler');
-    if (!script || !script.getAttribute) return;
-    const options = getOptions(script as HTMLScriptElement)
+interface MiniProfilerState {
+  results: Result[]
+}
 
-    const resultById: {[key: string]: Promise<Result>} = Object.create(null)
+class Details extends Component<MiniProfilerState, void> {
+  rowStyle = {
+    width: 300,
+    height: 24,
+    fontSize: '10px',
+    lineHeight: '24px',
+    color: '#F2F2F2',
+    fontFamily: 'Monaco, Courier, monospace',
+    display: 'flex',
+    pointerEvents: 'auto'
+  }
 
-    const results: Result[] = []
+  oddRowStyle = {
+    ...this.rowStyle,
+    backgroundColor: '#222222'
+  }
 
-    function fetchResultOnce(id: string): Promise<Result> {
-      if (!resultById[id]) {
-        resultById[id] = fetchResult(id, options).then((result) => {
-          results.push(result)
-          render()
-          return result
-        })
-      }
-      return resultById[id]
-    }
+  evenRowStyle = {
+    ...this.rowStyle,
+    backgroundColor: '#050505'
+  }
 
-    addXhrOpenListener((xhr) => {
-      xhr.addEventListener('load', () => {
-        if (xhr.responseURL.indexOf(options.path) !== -1) return
-        const ids = xhr.getResponseHeader('X-MiniProfiler-Ids')
-        if (ids) {
-          const idList: string[] = JSON.parse(ids)
-          idList.forEach(fetchResultOnce)
-        }
-      })
-    })
-    fetchResultOnce(options.currentId)
+  durationStyle = {
+    width: 60,
+    display: 'block',
+    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+    paddingRight: 6,
+    textAlign: 'right'
+  }
+
+  urlStyle = {
+    display: 'block',
+    textOverflow: 'ellipsis',
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+    paddingLeft: 6,
+    flex: 1
+  }
+
+  boldStyle = {
+    fontWeight: 'bold'
+  }
+
+  renderResult = (result: Result, index: number) => {
+    return <div style={index % 2 === 0 ? this.evenRowStyle : this.oddRowStyle}>
+      <span style={this.durationStyle}>
+        <span style={this.boldStyle}>{Math.round(result.durationMs)}</span>ms
+      </span>
+      <span style={this.urlStyle}>
+        <span style={this.boldStyle}>{result.method}</span>
+        {' '}
+        {result.urlPath}
+      </span>
+    </div>
   }
 
   render() {
-    return null
+    return <div>{this.props.results.map(this.renderResult)}</div>
+  }
+}
+
+class EntryPoint extends Component<MiniProfilerState, void> {
+  style = {
+    width: 102,
+    height: 24,
+    fontFamily: 'Monaco, Courier, monospace',
+    fontSize: '10px',
+    lineHeight: '24px',
+    backgroundColor: '#050505',
+    color: '#E0E0E0',
+    textAlign: 'center',
+    userSelect: 'none',
+    pointerEvents: 'auto'
+  }
+
+  boldStyle = {
+    fontWeight: 'bold'
+  }
+
+  render() {
+    const max = Math.round(Math.max(...(this.props.results.map(res => res.durationMs))))
+    return <div style={this.style}>
+      <span style={this.boldStyle}>{max}</span>ms max/{this.props.results.length}
+    </div>
+  }
+}
+
+class MiniProfiler extends Component<{}, MiniProfilerState> {
+  private resultById: {[key: string]: Promise<Result>} = Object.create(null)
+  private options: Options
+
+  constructor() {
+    super()
+
+    const script = document.getElementById('mini-profiler');
+    if (!script || !script.getAttribute) return;
+    this.options = getOptions(script as HTMLScriptElement)
+
+    this.state = {
+      results: []
+    }
+  }
+
+  fetchResultOnce = (id: string): Promise<Result> => {
+    if (!this.resultById[id]) {
+      this.resultById[id] = fetchResult(id, this.options).then((result) => {
+        this.setState({results: [...this.state.results, result]})
+        return result
+      })
+    }
+    return this.resultById[id]
+  }
+
+  componentDidMount() {
+    addXhrOpenListener((xhr) => {
+      xhr.addEventListener('load', () => {
+        if (xhr.responseURL.indexOf(this.options.path) !== -1) return
+        const ids = xhr.getResponseHeader('X-MiniProfiler-Ids')
+        if (ids) {
+          const idList: string[] = JSON.parse(ids)
+          idList.forEach(this.fetchResultOnce)
+        }
+      })
+    })
+    this.fetchResultOnce(this.options.currentId)
+  }
+
+  style = {
+    zIndex: Number.MAX_SAFE_INTEGER - 10,
+    width: '100vw',
+    height: '100vh',
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    pointerEvents: 'none',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    flexDirection: 'column'
+  }
+
+  render() {
+    const {results} = this.state
+    if (results.length === 0) {
+      return null
+    }
+    return <div style={this.style}>
+      <Details results={results} />
+      <EntryPoint results={results} />
+    </div>
   }
 }
 
